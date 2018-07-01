2139fd009978fd0170e373be374a58374c710fe5
Phil Nash <philnash@gmail.com>
2018-07-01 18:30:47 +1000

    Uses Rollup to create distinct modules for different environments. Uses Babel to compile to es5.

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import pkg from './package.json';
+import babel from 'rollup-plugin-babel';
+
+export default [
+  // browser-friendly UMD build
+  {
+    input: 'src/index.js',
+    output: {
+      name: 'WebShareWrapper',
+      file: pkg.browser,
+      format: 'umd'
+    },
+    plugins: [
+      babel({
+        exclude: 'node_modules/**'
+      })
+    ]
+  },
+
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // an array for the `output` option, where we can specify
+  // `file` and `format` for each target)
+  {
+    input: 'src/index.js',
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ],
+    plugins: [
+      babel({
+        exclude: 'node_modules/**'
+      })
+    ]
+  }
+];
